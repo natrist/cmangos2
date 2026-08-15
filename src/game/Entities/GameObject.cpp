25d1d0f228938778a87f0f8092b6d3c7adf94912
Tristan Cormier <cormiert2@outlook.com>
2026-08-15 14:26:25 -0400

    chore(entities): add default switch case in GameObject::CanUseNow

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -1510,7 +1510,9 @@ bool GameObject::CanUseNow(Player const* player) const
             }
             break;
         }
-        default: break;
+
+        default:
+            break;
     }
 
     if (!GetGOInfo()->GetLockId())
